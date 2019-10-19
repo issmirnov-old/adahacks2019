fa611d8c7202b51052f12e21cb193aea60204c01
Glitch (adahacks2019) <none>
2019-10-19 21:07:46 +0000

    latest snapshot

--- server.js
+++ server.js
@@ -3,48 +3,32 @@
 
 // init project
 const express = require("express");
-var PouchDB = require('pouchdb-node');
+var helmet = require('helmet')
+
+var PouchDB = require("pouchdb-node");
 
 const app = express();
 
-// we've started you off with Express,
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
-
-var db = new PouchDB('todos');
-var remoteCouch = false;
-addTodo(db, "testing todo")
+// create pouchdb database in .data
+var TempPouchDB = PouchDB.defaults({prefix: '.data'})
 
 
-// http://expressjs.com/en/starter/static-files.html
+app.use(helmet()) // secure http headers.
+app.use('/db', require('express-pouchdb')(TempPouchDB));
+
+var db = new PouchDB("todoss");
+var remoteCouch = 'https://panoramic-chronometer.glitch.me/db';
+
+// tell express web server where our files live
 app.use(express.static("public"));
 
 // http://expressjs.com/en/starter/basic-routing.html
+// register express routes. you can add more pages here using different routes "/demo", etc.
 app.get("/", function(request, response) {
-  //response.sendFile(__dirname + "/views/index.html");
-  db.allDocs({include_docs: true, descending: true}, function(err, doc) {
-    //redrawTodosUI(doc.rows);
-      response.send(doc.rows);
-  });
-  
-
+  response.sendFile(__dirname + "/views/index.html"); // send our main html page
 });
 
-// listen for requests :)
+// listen for requests - this is what sends the file.
 const listener = app.listen(process.env.PORT, function() {
   console.log("Your app is listening on port " + listener.address().port);
 });
-
-
-
-function addTodo(db, text) {
-  var todo = {
-    _id: new Date().toISOString(),
-    title: text,
-    completed: false
-  };
-  db.put(todo, function callback(err, result) {
-    if (!err) {
-      console.log('Successfully posted a todo!');
-    }
-  });
-}
